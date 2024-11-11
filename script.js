9cdfbb6939d1074f682ddadcf0340881799282b1
Be30nCH1k <danildudin592@gmail.com>
2024-11-11 12:07:52 +0300

    Добавил бургер меню

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ const container = document.getElementById('data-list')
 const URL = 'https://6729bdac6d5fa4901b6e27f4.mockapi.io/attractions';
 
 
-const option = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-}
+
 
 window.addEventListener('load', async ( ) => {
     const data = await fetch(URL);
@@ -19,10 +15,9 @@ window.addEventListener('load', async ( ) => {
         persone.map((p, index) => {
             const node = document.createElement('div');
             node.insertAdjacentHTML('afterbegin', `
-            <img src="${p.photo}" alt="img ${index + 1}">
+            <img src="${p.photo}" alt="PressF картинки ${index + 1}">
+            <h3> Gender: <i>${p.gender}</i></h3>
             <h3>${p.firstName} ${p.lastName}</h3>
-            <h4 class="h4">Date of birth: ${new Date(p.birthDate).
-            toLocaleDateString("en", option)}<h4>
             <span> Country: <i>${p.county}</i></span>
             <hr/>
             `)
